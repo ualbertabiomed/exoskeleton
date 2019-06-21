256bde3e26e862457e61665bb321424084c3ce9c
Taylor Zowtuk <zowtukt@gmail.com>
2019-06-21 17:43:41 -0600

    Implemented a sketch which uses a custom main

--- arduino/testMain/testMain.ino
+++ arduino/testMain/testMain.ino
@@ -1,3 +1,7 @@
+// Use the following link for info on how to overwrite main
+// http://gammon.com.au/forum/?id=12625
+
+#include <Arduino.h>
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
@@ -5,8 +9,34 @@ Servo myservo;  // create servo object to control a servo
 
 int pos = 0;    // variable to store the servo position
 
+int main(void)
+{
+  init();
+
+  // Dont know what this does but if uncommented results in error saying multiple mains
+  //initVariant();  
+  // "The problem is that initVariant is in the same module as main.  
+  // Calling initVariant causes the linker to include all of main.cpp."
+
+#if defined(USBCON)
+  USBDevice.attach();
+#endif
+  
+  setup();
+    
+  for (;;) {
+    loop();
+    if (serialEventRun) serialEventRun();
+  }
+        
+  return 0;
+}
+
 void setup() {
   myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
@@ -15,6 +45,13 @@ void loop() {
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+  
+  // flash the onboard led on and off btwn rotations so we know that we successfully used our main
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
