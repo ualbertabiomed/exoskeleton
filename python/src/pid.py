ba93ce8289096704a662974ec2a422efab3e356f
Ravenshard <jechmila@ualberta.ca>
2019-10-04 19:07:53 -0600

    PID testing and some interesting things are happening

--- python/src/pid.py
+++ python/src/pid.py
@@ -1,3 +1,17 @@
+import rospy
+import matplotlib.pyplot as plt
+
+
+"""
+NOTE:
+Very difficult to test.  The time will vary slightly (i.e. by 0.00001 normally)
+This makes the end calculation vary a little as well (i.e. by 0.00001).
+Compounded over time, these differences will become larger.
+
+
+"""
+
+
 class PID:
     def __init__(self, Kp=0.001, Ki=0, Kd=0):
         ''' init the PID control, initialize all variables we will need
@@ -17,6 +31,7 @@ class PID:
         self.Ki = None
         self.Kd = None
         self.err = None
+        self.prevErr = None
         self.totalErr = 0
         self.derv = None
         return
@@ -48,10 +63,10 @@ class PID:
              Kp * Current Error + Ki * Integral + Kd * Derivative
         '''
         self.cTime = rospy.get_time()
-        self.duration = cTime - self.initTime
+        self.duration = self.cTime - self.initTime
         self.initTime = self.cTime
         self.err = err
-        return calcP() + calcI() + calcD()
+        return self.calcP() + self.calcI() + self.calcD()
 
     def calcP(self):
         ''' Calculate the P portion of PID
@@ -90,4 +105,41 @@ class PID:
             self.derv = float((self.err-self.prevErr)/self.duration)
         else:
             self.derv = 0
+        self.prevErr = self.err
         return self.Kd*self.derv
+
+
+def testPID():
+    '''NOTE:
+    This can be played with to see the difference in the constants
+    Ki should be approx. 1e5 and Kd should be approx. 1e-5
+    This is because duration is approx. 1e-5, and Ki is multplied by duration
+    and Kd is divded by duration.  The orders balance each other
+    '''
+    rospy.init_node('PID_Tester')
+    controller = PID()
+    controller.setConstants(0.55, 20000, 0.000005)
+    goal = 0.3
+    curPos = 0.0
+    counter = 0
+    posQueue = list()
+    posQueue.append(curPos)
+    while counter < 100:
+        curPos += controller.calcPID(goal-curPos)
+        posQueue.append(curPos)
+        # print("curPos: {} \t err: {} \t duration: {}".format(curPos, goal-curPos, controller.duration))
+        # print("P: {} \t I: {} \t D: {}".format(controller.Kp*controller.err,
+                # controller.Ki*controller.totalErr, controller.Kd*controller.derv))
+        counter += 1
+        if curPos > 100:
+            break
+    plt.clf()
+    plt.plot([x for x in range(len(posQueue))], posQueue)
+    plt.plot([x for x in range(len(posQueue))], [goal for x in range(len(posQueue))])
+    plt.show()
+    return
+
+
+
+if __name__ == '__main__':
+    testPID()
